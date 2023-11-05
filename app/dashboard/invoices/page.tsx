d26d5391ccc8e3b91fe3217d3fddd85209b6a3b2
Владислав Фадеев <fadeevvladislavw@gmail.com>
2023-11-05 17:45:47 +0300

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+   return <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam neque praesentium necessitatibus architecto iste laborum fugiat tempore laboriosam explicabo, ea porro numquam culpa laudantium dolorum eaque repudiandae dolores accusantium quas?
+   </>
+}
